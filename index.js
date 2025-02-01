93fc9dd837dd58c33cb0dd67855df02c71172602
Aiswarya0025 <aiswaryan2006@gmail.com>
2025-02-01 15:50:57 +0530

    Added frontend

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const btnE1= document.getElementById("btn")
 const bmiInputE1 = document.getElementById("bmi-result")
-const weightConditionE1 =document.getElementById
-("we")
+const weightConditionE1 =document.getElementById("WeightCondition")
 function calculateBMI(){
     const heightValue = document.getElementById
     ("Height").value
@@ -13,11 +12,14 @@ function calculateBMI(){
     bmiInputE1.value = bmiValue
     
     if(bmiValue < 18.5){
-        weightConditionE1.innerText ="Under Weight"
-    } else if (bmiValue >=18.5 && bmiValue <= 24.9)
+        weightConditionE1.innerText ="Under Weight";
+    }else if (bmiValue >=18.5 && bmiValue <= 24.9)
     {
-    } else if (bmiValue >=18.5 && bmiValue <= 24.9)
-    {
+        weightConditionE1.innerText ="Normal Weight";
+    } else if (bmiValue >=25 && bmiValue <=29.9){
+        weightConditionE1.innerText ="Overweight";
+    } else if (bmiValue >=30){
+        weightConditionE1.innerText ="Obesity";
     }
 }
 
